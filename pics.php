ca22618ecb0053253f9fa037c4cbe39a0251f82a
Rishi <msrishi88@gmail.com>
2014-07-27 18:18:20 +0530

    success

--- pics.php
+++ pics.php
@@ -2,27 +2,26 @@
 
 </script>
 <?php       
-$path = '/wamp/www/instagram/';
+$path = '/wamp/www/instagram/';//set your path where you want the photos :)
     require_once 'instagram.class.php';
 	$tag=$_POST['instagram'];
 
     $instagram = new Instagram('814711a8545b471cbec145e280508048');
 
-
+	
     $media = $instagram->getTagMedia($tag);
 
-    $limit = 50;
-$f=1;
+    $limit = 50;//tried it but still instagram allows on;y 20 pics or lower than 20 :(
+	$f=1;
     // Set height and width for photos
     $size = '250';
 
-    // Show results
-    // Using for loop will cause error if there are less photos than the limit
+    // gets the pic with the required hashtag 
     foreach(array_slice($media->data, 0, $limit) as $data)
     {
-        // Show photo
-    //  echo '<p><img src="'.$data->images->thumbnail->url.'" ></p>'; 
+        // Show photo since $data->images->thumbnail->url gives the url of the photos present with the required hashtag
     $r='<img src="'.$data->images->thumbnail->url.'" height="'.$size.'" width="'.$size.'">';
+	//now it stores into the folder named instagram in the path you have given
 	$content = file_get_contents($data->images->thumbnail->url);
 	$fp = fopen("instagram/".$f.".jpg", "w");
 	fwrite($fp, $content);
